24e6fb83662e752ce216936bc2c942c438ca764d
Simon Mishchenko <symon261731@gmail.com>
2023-10-06 01:26:48 +0300

    my first working with console

--- main.go
+++ main.go
@@ -3,5 +3,10 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("hello, Golang!")
+	var name string
+	var age int
+	fmt.Print("Введите имя и возраст через пробел: ")
+	fmt.Scan(&name, &age)
+
+	fmt.Println("My name is", name, "and i am", age, "years old")
 }
