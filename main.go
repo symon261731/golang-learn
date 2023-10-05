156a1ed7142634d1bb1f5cfa8a34c92fce5a4dcb
Simon Mishchenko <symon261731@gmail.com>
2023-10-06 02:25:28 +0300

    objective

--- main.go
+++ main.go
@@ -3,10 +3,9 @@ package main
 import "fmt"
 
 func main() {
-	var name string
-	var age int
-	fmt.Print("Введите имя и возраст через пробел: ")
-	fmt.Scan(&name, &age)
+	var inNum int
 
-	fmt.Println("My name is", name, "and i am", age, "years old")
+	fmt.Scan(&inNum)
+
+	fmt.Println("It is", inNum/30, "hours", 2*(inNum%30), "minutes")
 }
