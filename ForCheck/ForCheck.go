fa4b9bb3d3f317c7bfcd60aae407b58b4a4739ae
Simon Mishchenko <symon261731@gmail.com>
2024-02-19 01:01:57 +0300

    done

--- ForCheck/ForCheck.go
+++ ForCheck/ForCheck.go
@@ -1,39 +1,106 @@
 package ForCheck
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
-func isOddNumber(num int) bool {
+// 1
+func IsOddNumber(num int) bool {
 	if num%2 == 0 {
 		return true
 	}
 	return false
 }
 
-type CoordinatesStruct struct {
-	x int
-	y int
-}
-
-func CalcCoordinates() CoordinatesStruct {
+// 2
+func CalcCoordinates() (int, int) {
 	var x, y int
 	fmt.Println("Введите пожалуйста значение X")
 	fmt.Scan(&x)
 	fmt.Println("Введите пожалуйста значение Y")
 	fmt.Scan(&y)
-	var result = prepareCoordinates(x, y)
 
-	return result
+	return prepareCoordinates(x, y)
 }
 
 // Вроде будет недоступна вне пакета
-func prepareCoordinates(x int, y int) CoordinatesStruct {
+func prepareCoordinates(x int, y int) (int, int) {
 	var resultX = 2*x + 10
 	var resultY = -3*y - 5
 
-	var coordinates CoordinatesStruct
-	coordinates.x = resultX
-	coordinates.y = resultY
+	return resultX, resultY
+}
 
-	return coordinates
+// 3
+func CalcAddAndMultiplyResult(num int) (int, int) {
+	return multiplyNumber(num), addNumber(num)
+}
 
+func multiplyNumber(num int) int {
+	var multiplyNum int
+	fmt.Println("На сколько умножить")
+	fmt.Scan(&multiplyNum)
+	return multiplyNum * num
+}
+
+func addNumber(number int) int {
+	var addNumber int
+	fmt.Println("Сколько прибавить")
+	fmt.Scan(&addNumber)
+	return addNumber + number
+}
+
+// 4
+var globalVariable = 5
+
+func CalcResultOfSumFunctions() int {
+	var callFirstFunction = addFirstFunction(globalVariable)
+	var callSecondFunction = addSecondFunction(callFirstFunction)
+	var callThirdFunction = addThirdFunction(callSecondFunction)
+
+	return callThirdFunction
+}
+
+func addFirstFunction(num int) int {
+	return num + 5
+}
+
+func addSecondFunction(num int) int {
+	return num + 6
+}
+func addThirdFunction(num int) int {
+	return num + 8
+}
+
+//5
+
+func CalcSumOfOddAndEvenNumberInArray() ([]int, error) {
+	var array = make([]int, 10)
+
+	for i := 0; i < 10; i++ {
+		var num int
+
+		fmt.Printf("Введите число для индекса %d \n", i)
+		num, err := fmt.Scan(&num)
+		if err != nil {
+			return nil, errors.New("error")
+		}
+		array[i] = num
+	}
+
+	return array, nil
+}
+
+func ReverseArray(arr []int) []int {
+	return processReverseArray(arr)
+}
+
+func processReverseArray(arr []int) []int {
+	var result = make([]int, len(arr))
+
+	for i := 0; i < len(arr); i++ {
+		result[i] = arr[len(arr)-i-1]
+	}
+	return result
 }
