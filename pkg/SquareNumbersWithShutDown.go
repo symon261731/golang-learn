65434d059cb01bbe1b6291b156653548af7b866c
Simon Mishchenko <symon261731@gmail.com>
2024-07-12 17:53:12 +0300

    refactor

--- pkg/SquareNumbersWithShutDown.go
+++ pkg/SquareNumbersWithShutDown.go
@@ -5,16 +5,14 @@ import (
 	"main/pkg/utils"
 	"os"
 	"os/signal"
-	"sync"
 	"syscall"
 	"time"
 )
 
 func SquareNumbersWithShutdown() {
-	// как бы переделать на еще один канал
-	var waitGroup sync.WaitGroup
 	var num = 1
 	var signalChannel = make(chan os.Signal, 1)
+	signal.Notify(signalChannel, syscall.SIGINT)
 	exit := false
 
 	// запускается перед всем началом цикла, и начинает ожидать значения
@@ -22,28 +20,17 @@ func SquareNumbersWithShutdown() {
 	go utils.WaitExit(signalChannel, &exit)
 
 	for {
-		// добавляем 1 в счеткик очереди для контроля горутин
-		waitGroup.Add(1)
-
-		signal.Notify(signalChannel, syscall.SIGINT)
-
-		go SquareNumber(&waitGroup, num)
-		// ниже идет ожидание выполнения горутины т.е отработки defer wg.done()
-		waitGroup.Wait()
-		num++
-
 		if exit {
 			break
 		}
-	}
+		SquareNumber(num)
+		num++
 
-	// тут вроде ставим чтобы последняя горутина с возведением в квадрат выполнилась когда когда мы уже отсигналили
-	waitGroup.Wait()
+	}
 
 }
 
-func SquareNumber(wg *sync.WaitGroup, num int) {
-	defer wg.Done()
+func SquareNumber(num int) {
 	time.Sleep(1 * time.Second)
 	fmt.Println(num * num)
 }
